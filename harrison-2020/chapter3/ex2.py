b0a2f2f5f71cdc2975c61029b78fa40120456be1
luoqin <luoqin.liu@utwente.nl>
2021-03-20 18:16:36 +0100

    finish chapter3

---
+++ harrison-2020/chapter3/ex2.py
@@ -0,0 +1,12 @@
+import numpy as np
+import nnfs
+import matplotlib.pyplot as plt
+
+from nnfs.datasets import spiral_data
+
+nnfs.init()
+
+X, y = spiral_data(samples=200, classes=3)
+
+plt.scatter(X[:,0], X[:,1], c=y, cmap='brg')
+plt.savefig("spiral.png")
